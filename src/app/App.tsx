a23e0067c33720c990dad86b5ea84b0e9a321b64
hamidjonashuraliev <khamidjonashuraliev033@gmail.com>
2024-05-03 18:22:10 +0900

    fix: store project standarts

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -5,19 +5,8 @@ import { RippleBadge } from "./MaterialTheme/styled";
 
 function App() {
     return (
-        <Container maxWidth="sm">
-            <Stack flexDirection={"column"}>
-                <Box sx={{ my: 4 }}>
-                    <Typography variant="h4" component={"h1"} gutterBottom>
-                        Create React App on typescript with REDUX
-                    </Typography>
-                </Box>
-                <Box>
-                    <RippleBadge badgeContent={4}>
-                        <Button variant="contained">Contained</Button>
-                    </RippleBadge>
-                </Box>
-            </Stack>
+        <Container >
+            
         </Container>
     );
 }
